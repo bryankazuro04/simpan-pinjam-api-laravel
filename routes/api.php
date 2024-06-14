a6ac7b43fb80e3a8ea42add8a3eef347055e8ac6
bryankazuro04 <mhdalana211201@gmail.com>
2024-06-14 15:55:26 +0700

    adding routing

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\SettingBungaController;
+use App\Http\Controllers\JenisTransaksiController;
+use App\Http\Controllers\TabunganAnggotaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('api')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', [UserController::class, 'get_user']);
+
+    Route::resource('anggota', AnggotaController::class);
+    Route::resource('jenis-transaksi', JenisTransaksiController::class);
+    Route::resource('tabungan', TabunganAnggotaController::class);
+
+    Route::get('/saldo/(:id)', [TabunganAnggotaController::class, 'saldo/$1']);
+
+    Route::get('/setting-bunga', [SettingBungaController::class, 'index']);
+    Route::get('add-setting-bunga', [SettingBungaController::class, 'addSettingBunga']);
 });
